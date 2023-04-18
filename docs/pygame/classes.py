706a5c564a1e9acda65eee0e63286fa19faa2c88
tpenha05 <tpenha30@gmail.com>
2023-04-18 14:40:14 -0300

    programa certo

--- docs/pygame/classes.py
+++ docs/pygame/classes.py
@@ -2,16 +2,10 @@ import pygame
 
 class Jogo:
     def __init__(self):
-<<<<<<< HEAD
         pygame.init()
         self.imagem = pygame.image.load('docs\imagens\Inverno_att.png')
         self.imagem= pygame.transform.scale(self.imagem,(1313,409))
         self.window = pygame.display.set_mode((1313,409))
-=======
-        self.imagem = pygame.image.load('docs/imagens/Outono.png')
-        self.imagem= pygame.transform.scale(self.imagem,(1000,500))
-        self.window = pygame.display.set_mode((1000,500))
->>>>>>> 0002cfec01ab150eacc2ca0d12da006afd280ffb
 
     def desenha(self):
         # self.window.fill((0,0,0))
