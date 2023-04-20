6b465597212a99d3f3aafc596fbf501849e10c56
Joao-Gabriel05 <jgffaraco@gmail.com>
2023-04-20 15:42:11 -0300

    Tentando criar colisao com mask

--- docs/pygame/classes.py
+++ docs/pygame/classes.py
@@ -1,27 +1,15 @@
 import pygame
 import random
 class Plataforma(pygame.sprite.Sprite):
-    def __init__(self,tela):
+    def __init__(self,tela,x,y):
         pygame.sprite.Sprite.__init__(self)
         self.image=pygame.transform.scale(pygame.image.load('docs/imagens/plataforma.png'),(150,50))
+        self.rect=self.image.get_rect()
+        self.rect.topleft=(x,y)
         self.plataforma_altura=self.image.get_height()
         self.plataforma_largura=self.image.get_width()
         self.tela=tela
-        self.plataformas=[]
-        self.group=pygame.sprite.Group()
-        i=0
-        while i <5:
-            posicao_x = random.randint(0, 2450)
-            posicao_y = random.randint(200, 309)
-            self.rect = self.image.get_rect(bottomright=(posicao_x, posicao_y))
-            self.group.add(self)
-            #if not pygame.sprite.spritecollide(self,self.group,False):
-            self.plataformas.append([posicao_x, posicao_y])
-            i+=1
-
-    def desenha_plataforma(self):
-            for plataforma in self.plataformas:
-                self.tela.blit(self.image,(plataforma[0],plataforma[1]))
+        self.mask = pygame.mask.from_surface(self.image)
 
 class Chao(pygame.sprite.Sprite):
     def __init__(self,tela):
@@ -42,23 +30,39 @@ class Tela_Inicio:
     def desenha_Tela_Inicio(self):
         self.window.fill((0,0,0))
 
-class Tela_Inverno:
+class Tela_Inverno(pygame.sprite.Sprite):
+    
     def __init__(self,window):
-        pygame.init()
+        pygame.sprite.Sprite.__init__(self)
         self.imagem = pygame.image.load('docs/imagens/Inverno_att.png')
         self.imagem= pygame.transform.scale(self.imagem,(3000,410))
         self.arvore=pygame.transform.scale(pygame.image.load('docs/imagens/Arvore_Inverno.png'),(100,100))
         self.imprime_x =0
         self.window=window
         self.arvores=[]       
+        self.plataformaGroup=pygame.sprite.Group()
+        self.posicao_plat=[]
         for i in range(10):
             posicao_x = random.randint(0, 2470)
             self.arvores.append([posicao_x,260])
+        for i in range(5):
+            posicao_x = random.randint(0, 2450)
+            posicao_y = random.randint(200, 250)
+            plataforma=Plataforma(self.imagem,posicao_x,posicao_y)
+            for sprite in self.plataformaGroup:
+                if pygame.sprite.collide_mask(sprite,plataforma) == None:
+                    self.plataformaGroup.add(plataforma)
+            if len(self.plataformaGroup)==0:
+                self.plataformaGroup.add(plataforma)
+    
     def desenha_tela(self):
         self.window.blit(self.imagem,(self.imprime_x,0))
         for arvore in self.arvores:
             self.imagem.blit(self.arvore,(arvore[0],arvore[1]))
+        self.plataformaGroup.draw(self.imagem)
+
 class Personagem(pygame.sprite.Sprite):
+   
     def __init__(self,window,tela):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale((pygame.image.load('docs/imagens/personagem.png')),(50,50))
@@ -68,61 +72,67 @@ class Personagem(pygame.sprite.Sprite):
         self.rect=self.image.get_rect(bottomright=(self.window.get_width()//2, 360))
         self.gravidade=0
         self.tela=tela
-        self.plataforma=Plataforma(self.tela)
+        self.inverno=Tela_Inverno(self.window)
+        #self.plataforma=Plataforma(self.tela)
+        self.mask=pygame.mask.from_surface(self.image)
+
     def desenha_jogador(self):
-        self.gravidade+=0.05
+        self.gravidade+=0.8
         self.rect.y+=self.gravidade
         if self.rect.bottom>=360:
             self.rect.bottom=360
-        for sprite in self.plataforma.group.sprites():
-            if pygame.sprite.collide_rect(self,sprite):  
-                print('colidiu')
-                self.rect.bottom=sprite.rect.top
+        #for sprite in self.inverno.plataformaGroup.sprites():
+            #if pygame.sprite.collide_mask(self,sprite)!=None:  
+             #   self.rect.bottom=sprite.rect.top
         self.window.blit(self.image,self.rect)
         
 class Jogo:
+    
     def __init__(self):
+        pygame.init()
         self.window = pygame.display.set_mode((1000,409))
         self.window_largura=self.window.get_width()
         self.tela=Tela_Inverno(self.window)
         self.chao=Chao(self.tela.imagem)
         self.jogador = Personagem(self.window,self.tela)
         self.tela_inicio=Tela_Inicio(self.window)
-        self.plataforma=Plataforma(self.tela.imagem)
         self.direção=0
         self.tela_atual=0
+        
+    
     def atualiza_estado(self):
+        clock = pygame.time.Clock()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    self.jogador.velocidade_x +=2
+                    self.jogador.velocidade_x +=10
                     self.direção='direita'
                 elif event.key == pygame.K_LEFT:
-                    self.jogador.velocidade_x-=2
+                    self.jogador.velocidade_x-=10
                     self.direção='esquerda'
                 elif event.key==pygame.K_SPACE and self.jogador.rect.bottom>=360:
-                        self.jogador.gravidade=-4
+                        self.jogador.gravidade=-15
                 elif event.key==pygame.K_RETURN:
                     self.tela_atual=1
             elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
-                        self.jogador.velocidade_x+=2
+                        self.jogador.velocidade_x+=10
                     elif event.key == pygame.K_RIGHT:
-                        self.jogador.velocidade_x -=2  
+                        self.jogador.velocidade_x -=10
         if (self.tela.imprime_x<=-2000 and (self.direção=='direita' or self.jogador.rect.x>=self.window.get_width()//2)) or (self.tela.imprime_x>=0 and (self.direção=='esquerda' or self.jogador.rect.x<=self.window.get_width()//2 )):
             self.jogador.rect.x+=self.jogador.velocidade_x
         else:
             self.tela.imprime_x -= self.jogador.velocidade_x 
-        
+        clock.tick(80)
         return True
 
     def desenha_inicio(self):
         if self.tela_atual==1:
             self.tela.desenha_tela()
             self.chao.desenha_chao()
-            self.plataforma.desenha_plataforma()
             self.jogador.desenha_jogador()
         elif self.tela_atual==0:
            self.tela_inicio.desenha_Tela_Inicio()
